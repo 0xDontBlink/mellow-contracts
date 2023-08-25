0a318bb23b544bb8b3b6bb1c894b0ffa3bed8a64
0xDontBlink <79304106+0xDontBlink@users.noreply.github.com>
2023-08-25 22:40:51 +0100

    Update MellowBits.sol

--- MellowBits.sol
+++ MellowBits.sol
@@ -2,13 +2,21 @@
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+import { IFeeReader } from "./IFeeReader.sol";
 
 pragma solidity ^0.8.20;
 
 contract MellowBits is Ownable {
     address public mellowFeeAddress;
+    IFeeReader feeReaderAddress;
+    // Fees
     uint256 public mellowFeePercent;
     uint256 public creatorFeePercent;
+    uint256 public reflectionFeePercent;
+
+    uint256 public delta = 600000000000000; //approx $20 per share for testing
 
     constructor() Ownable(_msgSender()){}
 
@@ -20,38 +28,29 @@ contract MellowBits is Ownable {
     // SharesSubject => Supply
     mapping(address => uint256) public sharesSupply;
 
-    function setFeeAddress(address _feeAddress) public onlyOwner {
-        mellowFeeAddress = _feeAddress;
-    }
-
-    function setMellowFeePercent(uint256 _feePercent) public onlyOwner {
-        mellowFeePercent = _feePercent;
-    }
-
-    function setCreatorFeePercent(uint256 _feePercent) public onlyOwner {
-        creatorFeePercent = _feePercent;
-    }
-
-    function getPrice(uint256 supply, uint256 amount) public pure returns (uint256) {
-        uint256 sum1 = supply == 0 ? 0 : (supply - 1 )* (supply) * (2 * (supply - 1) + 1) / 6;
-        uint256 sum2 = supply == 0 && amount == 1 ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
-        uint256 summation = sum2 - sum1;
-        return summation * 1 ether / 16000;
+    function getPrice(uint256 supply, uint256 amount) public view returns (uint256) {
+        if (supply == 0) return delta * amount;
+        return (supply * delta);
     }
 
     function getBuyPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
-        return getPrice(sharesSupply[sharesSubject], amount);
-    }
-
-    function getSellPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
-        return getPrice(sharesSupply[sharesSubject] - amount, amount);
+        (,uint256 spotPrice,,,,) 
+            = feeReaderAddress.getBuyInfo(sharesSupply[sharesSubject], delta, amount, creatorFeePercent, mellowFeePercent);
+        return spotPrice;
     }
 
     function getBuyPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
-        uint256 price = getBuyPrice(sharesSubject, amount);
-        uint256 mellowFee = price * mellowFeePercent / 1 ether;
-        uint256 subjectFee = price * creatorFeePercent / 1 ether;
-        return price + mellowFee + subjectFee;
+        uint256 supply = sharesSupply[sharesSubject];
+        
+        (,,, uint256 inputValue,,) 
+            = feeReaderAddress.getBuyInfo(supply, delta, amount, creatorFeePercent, mellowFeePercent);
+        return inputValue;
+    }
+
+   function getSellPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
+        (,uint256 spotPrice,,,,) 
+            = feeReaderAddress.getBuyInfo(sharesSupply[sharesSubject], delta, amount, creatorFeePercent, mellowFeePercent);
+        return spotPrice;
     }
 
     function getSellPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
@@ -63,7 +62,7 @@ contract MellowBits is Ownable {
 
     function buyShares(address sharesSubject, uint256 amount) public payable {
         uint256 supply = sharesSupply[sharesSubject];
-        require(supply > 0 || sharesSubject == msg.sender, "Only the shares' subject can buy the first share");
+        require(supply > 0 || sharesSubject == msg.sender, "Only the bits' creator can buy the first share");
         uint256 price = getPrice(supply, amount);
         uint256 mellowFee = price * mellowFeePercent / 1 ether;
         uint256 subjectFee = price * creatorFeePercent / 1 ether;
@@ -78,11 +77,11 @@ contract MellowBits is Ownable {
 
     function sellShares(address sharesSubject, uint256 amount) public payable {
         uint256 supply = sharesSupply[sharesSubject];
-        require(supply > amount, "Cannot sell the last share");
+        require(supply > amount, "Cannot sell the last bit");
         uint256 price = getPrice(supply - amount, amount);
         uint256 mellowFee = price * mellowFeePercent / 1 ether;
         uint256 subjectFee = price * creatorFeePercent / 1 ether;
-        require(sharesBalance[sharesSubject][msg.sender] >= amount, "Insufficient shares");
+        require(sharesBalance[sharesSubject][msg.sender] >= amount, "Insufficient bits");
         sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] - amount;
         sharesSupply[sharesSubject] = supply - amount;
         emit Trade(msg.sender, sharesSubject, false, amount, price, mellowFee, subjectFee, supply - amount);
@@ -90,5 +89,25 @@ contract MellowBits is Ownable {
         (bool success2, ) = mellowFeeAddress.call{value: mellowFee}("");
         (bool success3, ) = sharesSubject.call{value: subjectFee}("");
         require(success1 && success2 && success3, "Unable to send funds");
+    }
+
+   function setFeeAddress(address _feeAddress) public onlyOwner {
+        mellowFeeAddress = _feeAddress;
+    }
+
+    function setFeeReaderAddress(IFeeReader _feeReaderAddress) public onlyOwner {
+        feeReaderAddress = _feeReaderAddress;
+    }
+
+    function setMellowFeePercent(uint256 _feePercent) public onlyOwner {
+        mellowFeePercent = _feePercent;
+    }
+
+    function setCreatorFeePercent(uint256 _feePercent) public onlyOwner {
+        creatorFeePercent = _feePercent;
+    }
+
+    function setDeltaAmount(uint256 _delta) public onlyOwner {
+        delta = _delta;
     }
 }
